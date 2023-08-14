3e2a06034a30ffaea5d6b0753e5f6ecf7d914918
TopCoderUZ #NamSU <topcoder1600@gmail.com>
2023-08-14 13:29:37 +0500

    css qo'shildi

--- news/models.py
+++ news/models.py
@@ -4,6 +4,7 @@ from django.db import models
 class Post(models.Model):
     title = models.CharField(max_length=200)
     text = models.TextField()
+    # mail = models.EmailField()
 
     def __str__(self):
         return self.title
